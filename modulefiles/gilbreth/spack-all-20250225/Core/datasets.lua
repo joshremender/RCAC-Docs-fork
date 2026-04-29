ef477f952a46ee692099a1a07127b24e4254dd04
Guangzhen Jin <jin456@purdue.edu>
2026-04-29 01:07:38 -0400

    Update modules.

--- modulefiles/gilbreth/spack-all-20250225/Core/datasets.lua
+++ modulefiles/gilbreth/spack-all-20250225/Core/datasets.lua
@@ -11,3 +11,15 @@ help([[
 
 -- All set
 prepend_path("MODULEPATH", "/opt/spack/datasets")
+pushenv("DATASETS_DIR","/depot/datasets")
+
+-- This if statement makes sure to display the message only during module loading
+if (mode() == "load") then
+  LmodMessage("                    Welcome to RCAC datasets!                       ")
+  LmodMessage("---------------------------------------------------------------------")
+  LmodMessage("Use command 'module avail' to check all available dataset modules.")
+  LmodMessage("Use command 'module spider xxx' to query dataset modules.")
+  LmodMessage("e.g. module spider nasa")
+  LmodMessage("Use command 'module help xxx' to check instructions on dataset usage.")
+  LmodMessage("e.g. module help igenomes/AGPv3/2023-04-26")
+end
