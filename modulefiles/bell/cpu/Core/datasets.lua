0015ebe20f3ff6e0cc354511bf90b4b838d2c36d
Guangzhen Jin <jin456@purdue.edu>
2026-04-30 01:06:20 -0400

    Update modules.

--- modulefiles/bell/cpu/Core/datasets.lua
+++ modulefiles/bell/cpu/Core/datasets.lua
@@ -1,6 +1,6 @@
 local app = "datasets"
 local version = "null"
-local contact = "zhan4429"
+local contact = "hkashgar"
 local license_type = "null	"
 
 whatis("This module enables a collection of datasets.")
@@ -11,3 +11,15 @@ help([[
 
 -- All set
 prepend_path("MODULEPATH", "/opt/spack/modulefiles/datasets")
+pushenv("DATASETS_DIR","/depot/datasets")
+
+-- This if statement makes sure to display the message only during module loading
+if (mode() == "load") then
+  LmodMessage("                    Welcome to RCAC datasets!                       ")
+  LmodMessage("---------------------------------------------------------------------")
+  LmodMessage("Use command 'module avail' to check all available dataset modules.")
+  LmodMessage("Use command 'module spider xxx' to query dataset modules.")
+  LmodMessage("e.g. module spider nasa")
+  LmodMessage("Use command 'module help xxx' to check instructions on dataset usage.")
+  LmodMessage("e.g. module help igenomes/AGPv3/2023-04-26")
+end
